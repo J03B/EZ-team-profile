ce1bea8ee06a3834ce3fbcd8189562d1277273e4
J03B <byucrazyfan@gmail.com>
2023-01-30 23:35:41 -0600

    Added the rest of the team member prompts and stored to globals

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ function isEmail(response) {
     }
     return 'Must be a valid email address';
 }
+function idAlreadyTaken(response) {
+    const isNum = isNumber(response);
+    if (isNum) {
+        if (idsTaken.includes(response)) {
+            return 'ID already taken. Choose a different ID.';
+        }
+        else {
+            return true;
+        }
+    }
+    return isNum;
+}
 
 // Program startup and welcome
 console.log("\nWelcome to the EZ Team Profile Builder. Let's get started!\n");
@@ -39,17 +51,115 @@ function init() {
 
     // Add an engineer to the team
     function addEngineer() {
+        // Go through prompts to get an engineer's information
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter the engineer's name:",
+                validate: (answer) => notEmpty(answer)
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the engineer's employee ID:",
+                validate: (answer) => idAlreadyTaken(answer)
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the engineer's email address:",
+                validate: (answer) => isEmail(answer)
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the engineer's GitHub username:",
+                validate: (answer) => notEmpty(answer)
+            }
+        ])
 
+        // Evaluate the answers to the quesitons to store to the team array
+        .then((answers) => {
+            const engineer = new Engineer(
+                answers.id,
+                answers.name,
+                answers.email,
+                answers.github
+            );
+            teamArray.push(engineer);
+            idsTaken.push(answers.id);
+            addTeamMember();
+        })
     }
 
     // Add an Intern to the team
     function addIntern() {
+        // Go through prompts to get an intern's information
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter the intern's name:",
+                validate: (answer) => notEmpty(answer)
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter the intern's employee ID:",
+                validate: (answer) => idAlreadyTaken(answer)
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter the intern's email address:",
+                validate: (answer) => isEmail(answer)
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the intern's school:",
+                validate: (answer) => notEmpty(answer)
+            }
+        ])
 
+        // Evaluate the answers to the quesitons to store to the team array
+        .then((answers) => {
+            const intern = new Intern(
+                answers.id,
+                answers.name,
+                answers.email,
+                answers.school
+            );
+            teamArray.push(intern);
+            idsTaken.push(answers.id);
+            addTeamMember();
+        })
     }
 
     // Choice to add another member of the team
     function addTeamMember() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'class',
+                message: 'Which type of team member would you like to add:',
+                choices: ['Engineer','Intern','Done adding team members']
+            }
+        ]).then((answer) => {
+            switch (answer.class) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+            
+                case 'Intern':
+                    addIntern();
+                    break;
 
+                default:
+                    renderTeam();
+            }
+        });
     }
     
     // Add manager 
